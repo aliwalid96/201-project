baede5f3ca752dbb49195d90f93c8829102870e7
aliwalid96 <ali-waleed96@hotmail.com>
2021-03-02 19:43:08 +0200

    added lab 03 details

--- js/app.js
+++ js/app.js
@@ -7,7 +7,61 @@ let myAge=prompt("do you know how old am I  ");
 let university=prompt("do you think I study in the university");
 let studying=prompt("do you think I can study alone  ");
 let improvement =prompt("do you think I am self learner ");
-let favcolor=prompt("do you know if the green is my  favourate color  ")
+let favcolor=prompt("do you know if the green is my  favourate color  ");
+
+let Weight=prompt("can guess what is my Weight ");
+let corectWeight=75;
+let myWeight=parseInt(Weight);
+
+let myhoppy=["reading","swimming","sport"];
+
+
+
+for(let a=0;a<7;a++){
+ let hopy=prompt("can you ges any of my  hoppies");
+
+for(let i=1;i<4;i++){
+   if(hopy==myhoppy[i]){
+   alert("you gused it ");
+   a=8;
+}
+}
+}
+alert ("my hobby are   "+myhoppy);
+
+
+if(myWeight!=corectWeight){
+   let counter=0;
+   while((myWeight!=corectWeight)&&counter<5){
+      myWeight=prompt("can guess what is my Weight ");
+      if(myWeight>50 && myWeight<60){
+         alert("you are far");
+      }else if(myWeight>61 && myWeight<80){
+         if(myWeight=75){
+            alert("you are corect") ;
+            counter=7;
+
+         }else{
+         alert("you are near the correct")
+         }
+      }
+      else{
+         alert("you are far")
+      }
+      counter++;
+   }
+   
+if(counter>5){
+   alert("the correct is 75")
+}
+
+
+
+}
+
+
+
+
 checker(myAge);
 checker(university);
 checker(studying);
